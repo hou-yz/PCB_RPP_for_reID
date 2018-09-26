c90bbd563333f7f9ebc0d470da4761cdd4f9b726
hou_yz <hou_yz@outlook.com>
2018-09-27 01:32:54 +0800

    Sync

--- train_PCB.sh
+++ train_PCB.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=6,7 python PCB.py -d market -a resnet50 -b 64 -j 4 --epochs 60 --log logs/market-1501/PCB/ --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir /home/wangzd/houyz/open-reid-PCB_n_RPP/data/market1501/raw/Market-1501-v15.09.15
+CUDA_VISIBLE_DEVICES=6,7 python PCB.py -d market -a resnet50 -b 64 -j 4 --epochs 60 --log logs/market-1501/PCB/ --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir data/market/Market-1501-v15.09.15
